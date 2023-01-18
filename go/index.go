4200bf80ca6c7c6a2a154fbe6545f8ade6423327
kelvin abache <kelvinabache12@gmail.com>
2023-01-17 21:22:30 -0400

    terminamos el pregrama para transformar en json pero falta, la parte de go

--- go/index.go
+++ go/index.go
@@ -1,24 +1,23 @@
 // Tamabie tenemos que llamar un paqueta para empesar
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type datos struct {
-	name   string
-	number int
-	body   string
+	names     string
+	lastnames string
+	bodys     string
 }
 
+// func validador(name,lastname,body){
+// 	var dato datos
+// 	dato.names = name
+// 	dato.lastnames = lastname
+// 	dato.bodys = body
+
+// 	if types. {
+
+// 	}
+// }
+
 func main() {
-
-	person := datos{"kelvin", 465548798, "hello"}
-
-	v := json.Marshal(person)
-	b := []byte(`{"name":"kevin","lastname":"abache","body":"hello"}`)
-
-	fmt.Println()
-
+	//validador("kelvin","ache","hello")
 }
--- js/index.js
+++ js/index.js
@@ -1,27 +1,65 @@
+
+function datos(nombre,apellido,objecto) {
+   //condiciones para validar los datos(haciendo validacion por parte)
+   if(typeof nombre === 'number')  return 'valor no valido';
+
+   else if(typeof apellido === 'number') return 'valor no valido';
+
+   else if(typeof objecto === 'number')  return 'valor no valido';
+
+   else {
+        const almacen = {nombres:nombre, apellidos:apellido, objectos:objecto};
+        return almacen
+   };
+};
+
+function datosJson(valores){
+   //  const {nombres,apellidos,objectos} = valores;
+   const cambiandoJson = JSON.stringify(valores);
+   console.log(cambiandoJson);
+};
+
+datosJson(datos("kelvin",2,"hello"));
+
+datosJson(datos("manuel","peres","como estas"));
+
+
+
 //funcion creadora de elementos 
 
-function creardorDeElemntos(name,lastname,body) {
-   //primero ponemos una condicion para verificar los elementos
-  if(typeof name === 'string' && typeof lastname === 'string'&& typeof body === 'string') return [name,lastname,body];
-  else return 'estos no son elementos tipo string';
-}
+// function creardorDeElemntos(name,lastname,body) {
+//    //primero ponemos una condicion para verificar los elementos
+//   if(typeof name === 'number' && typeof lastname === 'number'&& typeof body === 'number') return 'datos numericos no acectados';
+//   else return [name,lastname,body];
+// }
 
-function pasar([name,lastname,body] = creardorDeElemntos) {
-   const person = {
-      name: name,
-      lastname: lastname,
-      body:body
-   }
+// function pasar([name,lastname,body] = creardorDeElemntos) {
+//    const person = {
+//       name: name,
+//       lastname: lastname,
+//       body:body
+// }
 
- return person;
-}
+// function creadorDeJson(){
+
+//   if(typeof name === 'number' && typeof lastname === 'number' && typeof body === 'number') return {}
+//       else {
+//           const objectos = JSON.stringify(person);
+//          return objectos
+//       }
+//  }
+//   console.log(creadorDeJson());
+// }
+
+
+// pasar(creardorDeElemntos("kelvin","abache","hello"))
+
+// pasar(creardorDeElemntos("kelvin",2,5))
+
 
 
 /***
  * cosas que hacen para mini proyecto CREANDO DATOS JSON
- * 1) crear una funcion que ordene los datos mediante uso de objectos
  * 2) crear otra funcion que separe los datos, colocando un id a cada elemento
- * 3) crear una forma de mostrar los datos requeridos 
- * 4)poner una funcion que permita una conexion entre dos funciones
  * donde esta guardado el codigo (https://github.com/Kelvinbache/aprendiendo-GO-y-profundizando-conceptos-de-JS);
  */
