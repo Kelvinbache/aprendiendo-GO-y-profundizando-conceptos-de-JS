949ea61634d3f0dbcc56370c5096f9bfb92d0359
kelvin abache <kelvinabache12@gmail.com>
2023-01-19 09:03:53 -0400

    repenzar la forma del codigo y buscar, las falla de los datos

--- go/index.go
+++ go/index.go
@@ -1,23 +1,39 @@
 // Tamabie tenemos que llamar un paqueta para empesar
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
 type datos struct {
 	names     string
 	lastnames string
 	bodys     string
 }
 
-// func validador(name,lastname,body){
-// 	var dato datos
-// 	dato.names = name
-// 	dato.lastnames = lastname
-// 	dato.bodys = body
+func validador(name, lastname, body string) {
+	valor := reflect.ValueOf(name)
 
-// 	if types. {
+	if valor == reflect.ValueOf(name) {
+		panic("no podemos retornar un numero")
+	} else {
+		almacen := []datos{
+			{names: name, lastnames: lastname, bodys: body},
+		}
 
-// 	}
-// }
+		contenido, erro := json.Marshal(almacen)
+
+		if erro != nil {
+			panic(erro)
+		} else {
+			fmt.Println(string(contenido))
+		}
+	}
+}
 
 func main() {
-	//validador("kelvin","ache","hello")
+	validador("kelvin", "abache", "hello")
+	validador("kelvin", "abache", "hello")
 }
