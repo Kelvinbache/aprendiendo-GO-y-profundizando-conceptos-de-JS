8a628cc0430b40950a409e5f47ea0697554c1cd0
kelvin abache <kelvinabache12@gmail.com>
2023-02-04 08:21:13 -0400

    Esta es la practica del viernes de js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,73 @@
-/**
- * examen de los metos y conceptos del otro dia:
-
- * 1)Crea una forma vitual para pasar una frase y comparar los primeras letras de la frase 
- * 2)conversacion de entre persona (compara las frases de las dos persona  y devuelva true o false)
-
- 1(Comparador de primera letra) pasos a seguir:
- * crear un objecto donde los parametros sean frase1 y frases2
- * hacer una funcion donde se compare las primeras letra
- * depues pasar en consola (son igules o no son iguales)
-*/
-
-const frases = (frase1,frase2) =>{
-     if(frase1.startsWith("A") == true) return 'es igual';
-}
-
-
-console.log(frases("Aaaah"))
-
-
-/**
- * @stringsWith (puede devolver true o falso) 
- * @includes (permite conparal las frases o oraciones de variables)
- * @endsWith (puede devolver true o false)
+/** 
+ * @ new Number methods o nuevo metodos de numero
+ * @ new methods math o nuevo metodo matematico  
  */
 
-/*StrinsWith devuelve true o false dependiendo la condion que tiene, que es la primera letra es mayuscula en una oracion o nombre
-// const strin = 'Estoy aqui';
-// //estos Busca las letras mayusculas de una oracion o nombre 
-// console.log(strin.startsWith('Es'));
-// console.log(strin.startsWith('es',1))
+/* examen de aprendido de hoy
+   1)Crear un sistema donde pueda pasar un texto  y convierte en numero (calcular la raiz cuadrada del numero)
+   2)crear otro sistema donde puedas calcular un triangulo  
 */
 
-/*include podemos revisar si es igual a la oraciones o frases
 
-const frase = 'estoy fuera de liga';
-
- const liga = 'estoy en mi liga';
-
-//podemos comparar dos valores entre si con estos metodos 
- console.log(`"${frase}" ${frase.includes(liga) ? 'is': 'is not'}`);
-
+/*2) sistema mediante un objecto 
+* Crear una clase donde podamos pasar un texto y convertir en numero 
+* calcular los tres lados de un tringulo
+* retornar los valores de la clase 
 */
 
-/*endsWith compara las ultimas palabras una frase o oracion 
-const frase = 'kelvin tiene un telefono';
-// los para metros que resive es (el valor final de la oracion o frase)
-console.log(frase.endsWith('telefono'))
+class tringulo{
+     constructor(arriba,ezquierda,derecha){
+          this.arriba = arriba;
+          this.ezquierda = ezquierda;
+          this.derecha = derecha;
+     }
+}
+
+const numero = new tringulo("4","8","4");
+
+const arriba =  numero.arriba;
+const izuierda = numero.izuierda;
+const derecha = numero.derecha;
+
+function resultado() {
+ const numero1 = Number(arriba);
+ const numero2 = Number(izuierda);
+ const numero3 = Number(derecha);
+
+ return numero2 * numero3 - Math.PI(numero1) /180;
+}
+
+console.log(resultado());
+
+/*1) sistema de creasion de texto en numero 
+* usar una funcion donde pases un texto numerico
+* hacer una constante donde se convierta en numero 
+* retornar el valor atrior y el actual
+ 
+const sistema = (texto) => {
+  const numero = Number(texto);  // Estamos pasomos un texto que tiene un los numerico 
+  return `valor anterior: ${texto} | valor nuevo: ${Math.sqrt(numero)}`; // Despues hacemos la operacion entre valor de texto, retornamos un los resultados
+}
+
+console.log(sistema("8"));
+console.log(sistema("6"));
+console.log(sistema("78"));
+console.log(sistema("24848"));
+*/
+
+/*Primero iremos metodos matematicos y convertidor de numeros
+
+
+// const Numero = "4"; // Number es un meto que convierte los valores tipo texto en numero
+ 
+// if( typeof Numero == 'string') {  //podemos hacer gualquier tipo de comparacion entre los valor 
+//      const creador = Number(Numero); 
+//      console.log(creador + 5);
+// }
+
+*/
+/* metodos matematico nos permite tener todos los metodos matematicos 
+ 
+const numero = 8;
+console.log(Math.SQRT1_2 + numero)
 */
